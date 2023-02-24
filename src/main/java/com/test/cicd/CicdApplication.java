35af471a7632cfd5ff060e6d07191349b7724eb3
MinjuKim <kpoong02@naver.com>
2023-02-24 20:31:36 +0900

    jpa에 db연결이 안 되어서 빌드 실패 해결

--- src/main/java/com/test/cicd/CicdApplication.java
+++ src/main/java/com/test/cicd/CicdApplication.java
@@ -2,8 +2,9 @@ package com.test.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CicdApplication {
 
     public static void main(String[] args) {
