d05655b3505d7d11712c7c3b252c62887c7ffdb7
MinjuKim <kpoong02@naver.com>
2023-02-24 20:22:42 +0900

    RestController

--- src/main/java/com/test/cicd/controller/LoggingController.java
+++ src/main/java/com/test/cicd/controller/LoggingController.java
@@ -2,7 +2,9 @@ package com.test.cicd.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LoggingController {
 
     @Value("${logging-module.version}")
